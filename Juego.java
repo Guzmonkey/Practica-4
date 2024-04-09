c615ff8e2d1923e89e696739c46f1913b841c79b
LuisLopez0110 <165829624+LuisLopez0110@users.noreply.github.com>
2024-04-08 23:39:54 -0700

    Add files via upload

--- Juego.java
+++ Juego.java
@@ -1,31 +1,128 @@
-package com.mycompany.practica4;
-import java.util.Scanner;
-import java.util.*;
-
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class Juego {
-    ArrayList<Cartas> cartas;
-    ArrayList<Cartas> player1Hand;
-    ArrayList<Cartas> player2Hand;
-    ArrayList<Cartas> player3Hand;
-    ArrayList<Cartas> player4Hand;
-    ArrayList<Cartas> player5Hand;
-    ArrayList<Cartas> player6Hand;
-    ArrayList<Cartas> playedCartas;
-    int players;
-    int[] sidePlayers;
-    boolean winner;
-    int opt;
-    Scanner sc = new Scanner(System.in);
+
+    private Baraja baraja;
+    private ArrayList<Carta> cartasJug1;
+    private ArrayList<Carta> cartasJug2;
+    private ArrayList<Carta> cartasJug3;
+    private ArrayList<Carta> cartasJug4;
+    private int jugadores;
+
+    public Juego(){
+        baraja = new Baraja();
+        baraja.crearBaraja();
+        baraja.revolverBaraja();
+        cartasJug1 = new ArrayList<>();
+        cartasJug2 = new ArrayList<>();
+        cartasJug3 = new ArrayList<>();
+        cartasJug4 = new ArrayList<>();
+    }
+
+    public void repartirCartas(){
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Ingresa el numero de jugadores");
+            jugadores = scanner.nextInt();
+            switch (jugadores) {
+                case 2:
+                    cartasJug1.addAll(baraja.getBarajaCartas().subList(0, 24));
+                    cartasJug2.addAll(baraja.getBarajaCartas().subList(24, 48));
+                    break;
+                case 3:
+                    cartasJug1.addAll(baraja.getBarajaCartas().subList(0, 16));
+                    cartasJug2.addAll(baraja.getBarajaCartas().subList(16, 32));
+                    cartasJug3.addAll(baraja.getBarajaCartas().subList(32, 48));
+                    break;
+                case 4:
+                    cartasJug1.addAll(baraja.getBarajaCartas().subList(0, 12));
+                    cartasJug2.addAll(baraja.getBarajaCartas().subList(12, 24));
+                    cartasJug3.addAll(baraja.getBarajaCartas().subList(24, 36));
+                    cartasJug4.addAll(baraja.getBarajaCartas().subList(36, 48));
+                    break;
+                default:
+                    System.out.println("Numero de jugadores invalido");
+                    break;
+            }
+
+        } while (jugadores < 2 || jugadores > 4);
+    }
+
+    // public void mostrarCartasJug(){
+    //     switch (jugadores) {
+    //         case 2:
+    //             System.out.println("Cartas del jugador 1");
+    //             for (Carta carta : cartasJug1){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 2");
+    //             for (Carta carta : cartasJug2){
+    //                 carta.mostrar();
+    //             }
+    //             break;
+    //         case 3:
+    //             System.out.println("Cartas del jugador 1");
+    //             for (Carta carta : cartasJug1){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 2");
+    //             for (Carta carta : cartasJug2){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 3");
+    //             for (Carta carta : cartasJug3){
+    //                 carta.mostrar();
+    //             }
+    //             break;
+    //         case 4:
+    //             System.out.println("Cartas del jugador 1");
+    //             for (Carta carta : cartasJug1){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 2");
+    //             for (Carta carta : cartasJug2){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 3");
+    //             for (Carta carta : cartasJug3){
+    //                 carta.mostrar();
+    //             }
+    //             System.out.println("Cartas del jugador 4");
+    //             for (Carta carta : cartasJug4){
+    //                 carta.mostrar();
+    //             }
+    //             break;
+    //     }
+    // }
+
+    public void mostrarCartasJug() {
+        for (int i = 0; i < jugadores; i++) {
+            System.out.println("Cartas del jugador " + (i + 1) + ":");
+            ArrayList<Carta> cartasJugador = null;
+            switch (i) {
+                case 0:
+                    cartasJugador = cartasJug1;
+                    break;
+                case 1:
+                    cartasJugador = cartasJug2;
+                    break;
+                case 2:
+                    cartasJugador = cartasJug3;
+                    break;
+                case 3:
+                    cartasJugador = cartasJug4;
+                    break;
+                default:
+                    break;
+            }
+            if (cartasJugador != null) {
+                for (Carta carta : cartasJugador) {
+                    carta.mostrar();
+                }
+            }
+        }
+    }
     
-    public static List<String> crearBaraja(){
-        ArrayList<String> cartas = new ArrayList<>();
-        String[] palo = {"Oro", "Copas", "Espadas", "Bastos"};
-        String[] valor = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-        return cartas;
-    }   
-    
-   public void repartirCartas(int players){
-       
-   }
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creamos una nueva baraja
+        Juego juego = new Juego();
+        juego.repartirCartas();
+        juego.mostrarCartasJug();
+    }
+}

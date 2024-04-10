4c298cc42690257c6dbc5f169ae0733a6411649e
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-10 09:50:58 -0700

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package guzmonkey.juegocartas;
 
-/**
- *
- * @author Gustavo
- */
+package gushdezzz.juegocartas;
+import java.util.ArrayList;
 public class Tablero {
+    private ArrayList<Cartas> escaleraBastos;
+    private ArrayList<Cartas> escaleraCopas;
+    private ArrayList<Cartas> escaleraEspadas;
+    private ArrayList<Cartas> escaleraOros;
     
+    public Tablero(){
+        this.escaleraBastos = new ArrayList<>();
+        this.escaleraCopas = new ArrayList<>();
+        this.escaleraEspadas = new ArrayList<>();
+        this.escaleraOros = new ArrayList<>();
+    }
+
+    public ArrayList<Cartas> getEscaleraBastos() {
+        return escaleraBastos;
+    }
+
+    public ArrayList<Cartas> getEscaleraCopas() {
+        return escaleraCopas;
+    }
+
+    public ArrayList<Cartas> getEscaleraEspadas() {
+        return escaleraEspadas;
+    }
+
+    public ArrayList<Cartas> getEscaleraOros() {
+        return escaleraOros;
+    }
+
 }

bbb43e59bb5e253f434ef5fa73781414cc5c5f90
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-11 09:41:34 -0700

    Update Carta.java

--- Carta.java
+++ Carta.java
@@ -1,52 +1,107 @@
-package guzmonkey.juegocartas;
-//Clase que modela una carta de la baraja espanola
-public class Carta {
-    //Variables de la clase 
+// Librerias necesarias
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.util.Scanner;
+import java.awt.Image;
+
+public class Cartas {
+    // Declaramos variables
+    private String figura;
     private int valor;
-    private String clase;
-    private boolean esVisible;
-
-    //Constructor de la clase Carta
-    public Carta(int valor, String clase, boolean esVisible){
+    private String ruta;
+    private boolean visible;
+    private ImageIcon icono;
+    /* 
+        Constructor de la clase Cartas
+    */
+    public Cartas(String figura, int valor){
+        this.figura = figura;
         this.valor = valor;
-        this.clase = clase;
-        this.esVisible = esVisible;
+        this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
+        this.visible = true;
+        this.icono = new ImageIcon(ruta);
     }
-
-    //Getters y Setters del valor
-    public int getValor(){
-        return valor;
+    /*
+        Sets y gets de la variable Figura
+    */
+    public void setFigura(String figura){
+        this.figura = figura;
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
+        }else{
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+        }
+        this.icono = new ImageIcon(ruta);
     }
-
+    
+    public String getFigura(){
+        return this.figura;
+    }
+    /*
+        Sets y gets de la variable Valor
+    */
     public void setValor(int valor){
         this.valor = valor;
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
+        }else{
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+        }
+        this.icono = new ImageIcon(ruta);
     }
-
-    //Getters y Setters de la clase
-    public String getClase(){
-        return clase;
+    
+    public int getValor(){
+        return this.valor;
     }
-
-    public void setClase(String clase){
-        this.clase = clase;
+    
+    /*
+        Sets y gets de la variable Visible
+    */
+    public void setVisible(boolean visible){
+        this.visible = visible;
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
+        }else{
+            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+        }
+        this.icono = new ImageIcon(ruta);
     }
-
-    //Getters y Setters de la clase
-    public boolean getEsVisible(){
-        return esVisible;
+    
+    public boolean getVisible(){
+        return this.visible;
     }
-
-    //Metodo para hacer visisble o no
-    public void setEsVisible(boolean esVisible){
-        this.esVisible = esVisible;
+    
+    public ImageIcon getIcono(){
+        return this.icono;
     }
-
-    //Metodo para mostrar en terminal
+    /*
+        Metodo para mostrar las cartas en terminal
+    */
     public void mostrar(){
-        if (esVisible){
-            System.out.println("[" + valor + "/" + clase + "]");
+        if (visible){
+            System.out.println("[" + valor + "/" + figura + "]");
         }else{
             System.out.print("[?/?] ");
         }
     }
+    
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ingrese la figura: ");
+    String figura = scanner.next();
+    System.out.println("Ingrese el valor: ");
+    int valor = scanner.nextInt();
+    Cartas carta = new Cartas(figura, valor);
+    
+    ImageIcon imagenIcono = new ImageIcon(carta.getIcono().getImage().getScaledInstance(108, 144, Image.SCALE_DEFAULT));
+    JLabel etiqueta = new JLabel(imagenIcono);
+    
+    JFrame ventana = new JFrame("Imagen de la carta");
+    ventana.setSize(210, 210);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.add(etiqueta);
+    ventana.setVisible(true);
+    }
+    
 }

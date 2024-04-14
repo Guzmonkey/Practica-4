3c811c6ca980479cb31ed92ae5ec8b4767a70043
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-13 17:54:03 -0700

    Update Carta.java
    DISEÑO DE CARTA CORREGIDO

--- Carta.java
+++ Carta.java
@@ -1,107 +1,89 @@
-// Librerias necesarias
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.util.Scanner;
-import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
 
-public class Cartas {
-    // Declaramos variables
-    private String figura;
+public class Carta {
     private int valor;
+    private String palo;
     private String ruta;
     private boolean visible;
     private ImageIcon icono;
-    /* 
-        Constructor de la clase Cartas
-    */
-    public Cartas(String figura, int valor){
-        this.figura = figura;
+
+    public Carta(String palo, int valor) {
+        this.palo = palo;
         this.valor = valor;
-        this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
+        this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
         this.visible = true;
         this.icono = new ImageIcon(ruta);
+        System.out.println(ruta);
     }
-    /*
-        Sets y gets de la variable Figura
-    */
-    public void setFigura(String figura){
-        this.figura = figura;
-        if (this.visible == true){
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
-        }else{
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+
+    public void setPalo() {
+        this.palo = palo;
+        if (this.visible == true) {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
+        } else {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-    
-    public String getFigura(){
-        return this.figura;
+
+    public String getPalo() {
+        return this.palo;
     }
-    /*
-        Sets y gets de la variable Valor
-    */
-    public void setValor(int valor){
+
+    public void setValor() {
         this.valor = valor;
-        if (this.visible == true){
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
-        }else{
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+        if (this.visible == true) {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
+        } else {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-    
-    public int getValor(){
+
+    public int getValor() {
         return this.valor;
     }
-    
-    /*
-        Sets y gets de la variable Visible
-    */
-    public void setVisible(boolean visible){
+
+    public boolean getVisible() {
+        return this.visible;
+    }
+
+    public void setVisible() {
         this.visible = visible;
-        if (this.visible == true){
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\" + figura + valor + ".jpg";
-        }else{
-            this.ruta = "C:\\Users\\Gustavo\\Documents\\NetBeansProjects\\juegoCartas\\src\\main\\java\\gushdezzz\\juegocartas\\imagenes\\folder\\volteada.jpg";
+        if (this.visible == true) {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
+        } else {
+            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-    
-    public boolean getVisible(){
-        return this.visible;
-    }
-    
-    public ImageIcon getIcono(){
+
+    public ImageIcon getIcon() {
         return this.icono;
     }
-    /*
-        Metodo para mostrar las cartas en terminal
-    */
-    public void mostrar(){
-        if (visible){
-            System.out.println("[" + valor + "/" + figura + "]");
-        }else{
-            System.out.print("[?/?] ");
-        }
-    }
-    
+
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Ingrese la figura: ");
-    String figura = scanner.next();
-    System.out.println("Ingrese el valor: ");
-    int valor = scanner.nextInt();
-    Cartas carta = new Cartas(figura, valor);
-    
-    ImageIcon imagenIcono = new ImageIcon(carta.getIcono().getImage().getScaledInstance(108, 144, Image.SCALE_DEFAULT));
-    JLabel etiqueta = new JLabel(imagenIcono);
-    
-    JFrame ventana = new JFrame("Imagen de la carta");
-    ventana.setSize(210, 210);
-    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ventana.add(etiqueta);
-    ventana.setVisible(true);
+        // Crear una nueva instancia de Carta
+        Carta carta = new Carta("bastos", 1);
+
+        // Crear el JFrame
+        JFrame ventana = new JFrame("Imagen carta");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Escalar la imagen al tamaño deseado
+        Image imagenEscalada = carta.getIcon().getImage().getScaledInstance(260, 398, Image.SCALE_SMOOTH);
+
+        // Crear un JLabel con la imagen escalada
+        JLabel etiqueta = new JLabel(new ImageIcon(imagenEscalada));
+
+        // Agregar la etiqueta al JFrame
+        ventana.getContentPane().add(etiqueta);
+
+        // Ajustar el tamaño del JFrame al tamaño de la imagen escalada
+        ventana.pack();
+
+        // Hacer visible el JFrame
+        ventana.setVisible(true);
     }
-    
 }

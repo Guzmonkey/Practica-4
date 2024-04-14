0e9dc9318a2012d3f1c376709518e287a57f685b
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-13 20:48:34 -0700

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -1,90 +1,175 @@
-import java.util.ArrayList;
-import java.awt.Image;
-import javax.swing.ImageIcon;
+package gushdezzz.maincartas;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 public class Tablero {
     private ArrayList<Carta> escaleraBastos;
     private ArrayList<Carta> escaleraCopas;
+    private ArrayList<Carta> escaleraOro;
     private ArrayList<Carta> escaleraEspadas;
-    private ArrayList<Carta> escaleraOros;
-    
+
     public Tablero(){
         this.escaleraBastos = new ArrayList<>();
         this.escaleraCopas = new ArrayList<>();
+        this.escaleraOro = new ArrayList<>();
         this.escaleraEspadas = new ArrayList<>();
-        this.escaleraOros = new ArrayList<>();
-    }
-
-    public ArrayList<Carta> getEscaleraBastos() {
-        return escaleraBastos;
-    }
-
-    public ArrayList<Carta> getEscaleraCopas() {
-        return escaleraCopas;
-    }
-
-    public ArrayList<Carta> getEscaleraEspadas() {
-        return escaleraEspadas;
-    }
-
-    public ArrayList<Carta> getEscaleraOros() {
-        return escaleraOros;
     }
     
-    public void rellenarTablero(){
-        for(int i = 0; i < 13; i++){
-            this.escaleraBastos.add(new Carta(i, "palos", false));
-            this.escaleraBastos.get(i-1).setVisible(false);
-            this.escaleraCopas.add(new Carta(i, "copas", false));
-            this.escaleraCopas.get( i- 1).setVisible(false);
-            this.escaleraEspadas.add(new Carta(i, "espadas", false));
-            this.escaleraEspadas.get(i - 1).setVisible(false);
-            this.escaleraOros.add(new Carta(i, "monedas", false));
-            this.escaleraOros.get(i - 1).setVisible(false);
-        }
-        
+    public ArrayList<Carta> getEscaleraBastos(){
+        return this.escaleraBastos;
     }
-    public void jugarCartas(Carta carta){
-        switch( carta.getClase()){
-            case "palos":
-            this.escaleraBastos.get(carta.getValor() - 1).setVisible(true);
-            break;
+    
+    public ArrayList<Carta> getEscaleraCopas(){
+        return this.escaleraCopas;
+    }
+    
+    public ArrayList<Carta> getOro(){
+        return this.escaleraOro;
+    }
+    
+    public ArrayList<Carta> getEspadas(){
+        return this.escaleraEspadas;
+    }
+    
+    public void llenarTablero(){
+        for(int i = 1; i < 11; i++){
+            this.escaleraBastos.add(new Carta("bastos", i));
+            this.escaleraBastos.get(i - 1).setVisible(false);
+            
+            this.escaleraCopas.add(new Carta("copas", i));
+            this.escaleraCopas.get(i - 1).setVisible(false);
+            
+            this.escaleraEspadas.add(new Carta("espadas", i));
+            this.escaleraEspadas.get(i - 1).setVisible(false);
+        
+            this.escaleraOro.add(new Carta("oro", i));
+            this.escaleraOro.get(i - 1).setVisible(false);
+        }
+    }
+    
+    public boolean validarCarta(Carta carta){
+        boolean flag = false;
+        switch(carta.getPalo()){
+            case "bastos":
+                if(carta.getValor() == 5){
+                    if(!this.escaleraBastos.get(4).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 1){
+                    if(!this.escaleraBastos.get(0).getVisible() && this.escaleraBastos.get(1).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 10){
+                    if(!this.escaleraBastos.get(9).getVisible() && this.escaleraBastos.get(8).getVisible()){
+                        flag = true;
+                    }
+                }else{
+                    if(!this.escaleraBastos.get(carta.getValor()-1).getVisible() && (this.escaleraBastos.get(carta.getValor()-2).getVisible() || 
+                            this.escaleraBastos.get(carta.getValor()).getVisible())){
+                        flag = true;
+                    }
+                }
+                break;
+            case "copas": 
+                if(carta.getValor() == 5){
+                    if(!this.escaleraCopas.get(4).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 1){
+                    if(!this.escaleraCopas.get(0).getVisible() && this.escaleraCopas.get(1).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 10){
+                    if(!this.escaleraCopas.get(9).getVisible() && this.escaleraCopas.get(8).getVisible()){
+                        flag = true;
+                    }
+                }else{
+                    if(!this.escaleraCopas.get(carta.getValor() - 1).getVisible() && (this.escaleraBastos.get(carta.getValor() - 2).getVisible() || 
+                            this.escaleraCopas.get(carta.getValor()).getVisible())){
+                        flag = true;
+                    }
+                } 
+                break;
+            case "oro":
+                if(carta.getValor() == 5){
+                    if(!this.escaleraOro.get(4).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 1){
+                    if(!this.escaleraOro.get(0).getVisible() && this.escaleraOro.get(1).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 10){
+                    if(!this.escaleraOro.get(9).getVisible() && this.escaleraOro.get(8).getVisible()){
+                        flag = true;
+                    }
+                }else{
+                    if(!this.escaleraOro.get(carta.getValor() - 1).getVisible() && (this.escaleraOro.get(carta.getValor() - 2).getVisible() 
+                            || this.escaleraOro.get(carta.getValor()).getVisible())){
+                        flag = true;
+                    }
+                }
+                break;
+            case "espadas":
+                if(carta.getValor() == 5){
+                    if(!this.escaleraEspadas.get(4).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 1){
+                    if(!this.escaleraOro.get(0).getVisible() && this.escaleraEspadas.get(1).getVisible()){
+                        flag = true;
+                    }
+                }else if(carta.getValor() == 10){
+                    if(!this.escaleraEspadas.get(9).getVisible() && this.escaleraEspadas.get(8).getVisible()){
+                        flag = true;
+                    }
+                }else{
+                    if(!this.escaleraEspadas.get(carta.getValor() - 1).getVisible() && (this.escaleraEspadas.get(carta.getValor() - 2).getVisible()
+                            || this.escaleraEspadas.get(carta.getValor()).getVisible())){
+                        flag = true;
+                    }
+                }
+                break;
+        }
+        return flag;
+    }
+
+    public void jugarCarta(Carta carta){
+        switch(carta.getPalo()){
+            case "bastos":
+                this.escaleraBastos.get(carta.getValor() - 1).setVisible(true);
+                break; 
             case "copas":
                 this.escaleraCopas.get(carta.getValor() - 1).setVisible(true);
-            break;
+                break;
+            case "oro":
+                this.escaleraOro.get(carta.getValor() - 1).setVisible(true);
+                break;
             case "espadas":
                 this.escaleraEspadas.get(carta.getValor() - 1).setVisible(true);
-            break;    
-            case "monedas":
-                this.escaleraOros.get(carta.getValor() - 1).setVisible(true);
-            break;
-        
+                break;
         }
     }
-    
     public JPanel getPanelBastos(){
         JPanel panel = new JPanel();
-        panel.setVisible(false);
         for(int i = 0; i < this.escaleraBastos.size(); i++){
-            ImageIcon icono = new ImageIcon(this.escaleraBastos.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
+            ImageIcon icono = new ImageIcon(this.escaleraBastos.get(i).getIcon().getImage().getScaledInstance(36,48,Image.SCALE_DEFAULT));
             JLabel etiqueta = new JLabel(icono);
-            etiqueta.setPreferredSize(new Dimension(36, 48));
+            etiqueta.setPreferredSize(new Dimension(38,48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
-            etiqueta.setVisible(true);
             panel.add(etiqueta);
         }   
         panel.setVisible(true);
         return panel;
     }
+    
     public JPanel getPanelCopas(){
         JPanel panel = new JPanel();
-        panel.setVisible(false);
         for(int i = 0; i < this.escaleraCopas.size(); i++){
-            ImageIcon icono = new ImageIcon(this.escaleraCopas.get(i).getIcono().getImage().getScaledInstance(36,48, Image.SCALE_DEFAULT));
+            ImageIcon icono = new ImageIcon(this.escaleraCopas.get(i).getIcon().getImage().getScaledInstance(36,48, Image.SCALE_DEFAULT));
             JLabel etiqueta = new JLabel(icono);
-            etiqueta.setPreferredSize(new Dimension(36, 48));
+            etiqueta.setPreferredSize(new Dimension(38, 48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
             etiqueta.setVisible(true);
@@ -93,34 +178,53 @@ public class Tablero {
         panel.setVisible(true);
         return panel;
     }
-    public JPanel getPanelEspadas(){
-        JPanel panel = new JPanel();
-        panel.setVisible(false);
-        for(int i = 0; i < this.escaleraEspadas.size(); i++){
-            ImageIcon icono = new ImageIcon(this.escaleraEspadas.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
-            etiqueta.setPreferredSize(new Dimension(36, 48));
-            panel.setBackground(new Color(53, 148, 100));
-            etiqueta.setBounds(100, 100, 36, 48);
-            etiqueta.setVisible(true);
-            panel.add(etiqueta);
-        }
-        panel.setVisible(true);
-        return panel;
-    }
+    
     public JPanel getPanelOro(){
         JPanel panel = new JPanel();
-        panel.setVisible(false);
-        for(int i = 0; i < this.escaleraOros.size(); i++){
-            ImageIcon icono = new ImageIcon(this.escaleraOros.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
+        for(int i = 0; i < this.escaleraOro.size(); i++){
+        ImageIcon icono = new ImageIcon(this.escaleraOro.get(i).getIcon().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
+        JLabel etiqueta = new JLabel(icono);
+        etiqueta.setPreferredSize(new Dimension(38, 48));
+        panel.setBackground(new Color(53, 148, 100));
+        etiqueta.setBounds(100, 100, 36, 48);
+        etiqueta.setVisible(true);
+        panel.add(etiqueta);
+        }
+        panel.setVisible(true);
+        return panel;
+    }
+    
+    public JPanel getPanelEspadas(){
+        JPanel panel = new JPanel();
+        for(int i = 0; i < this.escaleraEspadas.size(); i++){
+            ImageIcon icono = new ImageIcon(this.escaleraEspadas.get(i).getIcon().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));        
             JLabel etiqueta = new JLabel(icono);
-            etiqueta.setPreferredSize(new Dimension(36, 48));
-            etiqueta.setBackground(new Color(53, 148, 100));
+            etiqueta.setPreferredSize(new Dimension(38, 48));
+            panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
             etiqueta.setVisible(true);
             panel.add(etiqueta);
         }
+        panel.setVisible(true);
         return panel;
+    }
+    public static void main(String[] args){
+          Tablero tablero = new Tablero();
+        tablero.llenarTablero();
+
+        // Crear el JFrame
+        JFrame ventana = new JFrame("Tablero de cartas");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLayout(new GridLayout(1, 4)); // Un GridLayout de 1 fila y 4 columnas para los 4 palos
+
+        // Agregar paneles correspondientes a cada palo al JFrame
+        ventana.add(tablero.getPanelBastos());
+        ventana.add(tablero.getPanelCopas()); // Agrega paneles para otros palos aquí
+        ventana.add(tablero.getPanelOro());// Agrega paneles para los otros palos de cartas aquí
+        ventana.add(tablero.getPanelEspadas());
+        // Ajustar el tamaño del JFrame y hacerlo visible
+        ventana.pack();
+        ventana.setVisible(true);
     }
 }
 

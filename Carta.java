337adc0b4d357015ef8baa9b5d9877a59e4b1617
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-16 21:52:14 -0700

    Update Carta.java
    CORRECION DE LA CLASE CARTA

--- Carta.java
+++ Carta.java
@@ -1,95 +1,88 @@
 package gushdezzz.maincartas;
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.util.Scanner;
+import java.awt.Image;
 
 public class Carta {
+    
+    private String figura;
     private int valor;
-    private String palo;
     private String ruta;
     private boolean visible;
     private ImageIcon icono;
-
-    public Carta(String palo, int valor) {
-        this.palo = palo;
+    
+    public Carta(String figura, int valor){
+        this.figura = figura;
         this.valor = valor;
-        this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
+        this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\" + figura + valor + ".png";
         this.visible = true;
         this.icono = new ImageIcon(ruta);
-        System.out.println(ruta);
     }
-
+    
     public String getRuta(){
         return this.ruta;
     }
     
-    public void setPalo() {
-        this.palo = palo;
-        if (this.visible == true) {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
-        } else {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
+    public void setFigura(String figura){
+        this.figura = figura;
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\" + figura + valor + ".png";
+        }else{
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-
-    public String getPalo() {
-        return this.palo;
+    
+    public String getFigura(){
+        return this.figura;
     }
-
-    public void setValor() {
+    
+    public void setValor(int valor){
         this.valor = valor;
-        if (this.visible == true) {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
-        } else {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\" + figura + valor + ".png";
+        }else{
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-
-    public int getValor() {
+    
+    public int getValor(){
         return this.valor;
     }
-
-    public boolean getVisible() {
-        return this.visible;
-    }
-
-    public void setVisible(boolean visible) {
+    
+    public void setVisible(boolean visible){
         this.visible = visible;
-        if (this.visible == true) {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/" + palo + valor + ".png";
-        } else {
-            this.ruta = "C:/Users/guspa/OneDrive/Documents/NetBeansProjects/MainCartas/src/main/java//gushdezzz/imagenesCartas/volteada.png";
+        if (this.visible == true){
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\" + figura + valor + ".png";
+        }else{
+            this.ruta = "C:\\Users\\guspa\\OneDrive\\Desktop\\Cosas importantes\\Cuarto semestre\\Programacion orientada a objetos\\Laboratorio\\imagenesCartas\\volteada.png";
         }
         this.icono = new ImageIcon(ruta);
     }
-
     
-    public ImageIcon getIcon() {
+    public boolean getVisible(){
+        return this.visible;
+    }
+    
+    public ImageIcon getIcono(){
         return this.icono;
     }
-
-    public static void main(String[] args) {
-        // Crear una nueva instancia de Carta
-        Carta carta = new Carta("bastos", 1);
-
-        // Crear el JFrame
-        JFrame ventana = new JFrame("Imagen carta");
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la figura: ");
+        String figura = scanner.next();
+        System.out.println("Ingrese el valor: ");
+        int valor = scanner.nextInt();
+        Carta carta = new Carta(figura, valor);
+        JFrame ventana = new JFrame("Imagen de la carta");
+        ventana.setSize(110, 146);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Escalar la imagen al tamaño deseado
-        Image imagenEscalada = carta.getIcon().getImage().getScaledInstance(260, 398, Image.SCALE_SMOOTH);
-
-        // Crear un JLabel con la imagen escalada
-        JLabel etiqueta = new JLabel(new ImageIcon(imagenEscalada));
-
-        // Agregar la etiqueta al JFrame
-        ventana.getContentPane().add(etiqueta);
-
-        // Ajustar el tamaño del JFrame al tamaño de la imagen escalada
-        ventana.pack();
-
-        // Hacer visible el JFrame
+        JLabel etiqueta = new JLabel(new ImageIcon(carta.getIcono().getImage().getScaledInstance(108, 144, Image.SCALE_DEFAULT)));
+        ventana.add(etiqueta);
         ventana.setVisible(true);
     }
 }

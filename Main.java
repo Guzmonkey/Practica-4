3ac53a1259f3ae8a3c7f61701b27accb5e9e6721
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-04-19 21:51:20 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,15 @@
+
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        // Creamos una instancia del juego
-        Juego juego = new Juego();
-
-        // Repartimos las cartas
-        juego.repartirCartas();
-        
-        // Mostramos las cartas de cada jugador
-        juego.mostrarCartasJug();
-
-        // Ordenar las cartas de los jugadores en un Array
-        juego.ordenarCartasDeJugadores();
-
-        // Mostrar quien tiene el 5 de monedas
-        juego.encontrarJugador5DeMonesdas();
-
-        // Reorganizamos los jugadores
-        juego.reoganizarJugadores();
-
-        // Mostramos los jugadores ordenados
-        juego.imprimirCartasDeJugadoresOrdenadas();
-
-        // Jugar Cinquillo
-        juego.jugarCinquillo2();
-
-        // Mostrar las cartas jugadas
-        juego.mostrarCartasJugadas();
-
-        for (int i = 0; i < 15; i++) {
-            juego.jugarCinquillo2();    
-            juego.mostrarCartasJugadas();   
-        }
+    public static void main(String[] args){
+        // Declaramos variables
+        int jugadores;
+        Scanner sc = new Scanner(System.in);
+        // Pédimos el numero de jugadores
+        System.out.println("Ingrese el numero de jugadores:");
+        jugadores = sc.nextInt();
+        // Utilizamos otras clases
+        Juego cinquillo = new Juego(jugadores);
+        cinquillo.Jugar(jugadores);
     }
 }
